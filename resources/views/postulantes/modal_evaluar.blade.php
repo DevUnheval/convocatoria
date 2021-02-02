fe0b470e8997a1e160dbfd08073572493de8bd70
EmnerDev <77309667+EmnerDev@users.noreply.github.com>
2021-02-01 19:22:37 -0500

    modals postulantes

---
+++ resources/views/postulantes/modal_evaluar.blade.php
@@ -0,0 +1,29 @@
+<div class="modal fade" id="modal_evaluar" tabindex="-1" role="dialog"
+   aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header d-flex align-items-center">
+                <h4 class="modal-title" id="mySmallModalLabel">Evaluacion del Postulante</h4>
+                <button type="button" class="close ml-auto" data-dismiss="modal"
+                  aria-hidden="true">×</button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                      <label for="formGroupExampleInput">Evaluación Curricular</label>
+                      <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Curricular">
+                    </div>
+                    <div class="form-group">
+                      <label for="formGroupExampleInput2">Evaluación de Conocimientos</label>
+                      <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Conocimientos">
+                    </div>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput2">Evaluación de Entrevista</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Entrevista">
+                      </div>
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                  </form>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
