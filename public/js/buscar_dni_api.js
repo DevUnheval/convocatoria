960b650146a6ce36bba0d09313621dcaf7406d27
Franz Ahuanlla <franz.sistem@gmail.com>
2021-02-13 00:45:42 -0500

    verificacion de correo listo

--- public/js/buscar_dni_api.js
+++ public/js/buscar_dni_api.js
@@ -1,12 +1,30 @@
     $(document).ready(function(){
-        $('#dni').on('keyup',contardigitos)	
-        contardigitos();
+       // $('#dni').on('keyup',contardigitos);	
+
+        var forms = document.getElementsByClassName('needs-validation_registro');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+                form.classList.add('was-validated');
+                $('#form_registrar').removeClass('form-material');
+            }else{
+                               
+            }
+        
+            
+        }, false);
+    });
+
     });
     
     function contardigitos(){
         var dni = $('#dni').val();
         var count = $('#dni').val().length;
         if (count == 8) { //probar con dni de 6 dígitos '?
+        console.log('entre');
             $.get("/api_reniec/"+dni+"/dni",function(data){
                 $("#email").focus();
                 if (data != "error") {
