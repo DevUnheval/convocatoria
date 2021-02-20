733d572d41110ac7dedcfecad6ea3e78f9dc3a1b
slayerderk <30374923+slayerderk@users.noreply.github.com>
2021-02-19 21:59:47 -0500

    Update finpostular.blade.php

--- resources/views/postulante/finpostular.blade.php
+++ resources/views/postulante/finpostular.blade.php
@@ -4,39 +4,40 @@
 @endsection
 
 @section('content')
+<!--
 Estas postulando al proceso {{$proceso->cod}}<br>
 Sr(a): {{$datos_usuario->nombres}} {{$datos_usuario->apellido_paterno}} {{$datos_usuario->apellido_materno}}<br>
 RUC: {{$datos_usuario->ruc}}<br>
 Se ha enviado una contancia de su postulación al correo registrado <strong>{{$datos_usuario->email}}</strong><br>
-Puede ver su postulación ingresando <a href="#">Aquí</a>
-<div class="card border-bottom border-info">
+Puede ver su postulación ingresando <a href="#">Aquí</a> -->
+<div class="header bg-ligth-warning  alert alert-secondary col-md-7">
+    <h4 class="modal-title text-black text-center  font-weight-bold" id="fullWidthModalLabel">CONSTANCIA DE POSTULACIÓN</h4>
+</div>
+<div class="card border-left border-info col-md-7">
     <div class="card-body">
         <div class="d-flex align-items-center">
             <div>
-                <h2>Se guardo correctamente su postulación!</h2>
-                Estas postulando al proceso {{$proceso->cod}}<br>
-Sr(a): {{$datos_usuario->nombres}} {{$datos_usuario->apellido_paterno}} {{$datos_usuario->apellido_materno}}<br>
-RUC: {{$datos_usuario->ruc}}<br>
-Se ha enviado una contancia de su postulación al correo registrado <strong>{{$datos_usuario->email}}</strong><br>
-Puede ver su postulación ingresando <a href="#">Aquí</a>
-                <h6 class="text-info mb-0">News Feed</h6>
+                <!--<h2>CONSTANCIA DE POSTULACIÓN</h2> -->
+                <h4> <i class="fa fa-check" aria-hidden="true"></i> Se registro correctamente su postulación!</h4>
+
                 <div class="modal-body">                
                 <div class="card col-md-12">
                     <div class="row">
-                        <div class="col-2 border form-group">
-                            <img src="{{ asset(Auth::user()->img)}}" alt="user" width="80">
-                        </div>
-                        <div class="col-5 border form-group">
-                            <small class="text-center text-dark-info font-weight-bold "><strong>PROCESO: </strong></small> <br>{{$proceso->cod}}
-                               
-                            <br>
-                            <small><strong  class="text-dark-info font-weight-bold">N° Plazas = </strong> </small><br>{{$proceso->n_plazas}}
-                        </div>
-                        <div class="col-5 border form-group">
-                            <small ><strong class="text-center text-dark-info font-weight-bold">PUESTO AL QUE POSTULA:  </strong> </small><br>{{$proceso->nombre}}
-                            <br>
-                            <small ><strong class="text-center text-dark-info font-weight-bold">AREA/OFICINA:  </strong> </small> <br>{{$proceso->oficina}}
-                        </div>                     
+                        <div class="col-md-12 form-row">
+                            <div class="col-2 border form-group">
+                                <img src="{{ asset(Auth::user()->img)}}" alt="user" width="80">
+                            </div>
+                            <div class="col-5 border form-group">                                
+                                <small class="text-center text-dark-info font-weight-bold "><strong>ESTAS POSTULANDO AL PROCESO: </strong></small> <br>{{$proceso->cod}}
+                                <br>
+                                <small><strong  class="text-dark-info font-weight-bold">N° Plazas = </strong> </small><br>{{$proceso->n_plazas}}
+                            </div>
+                            <div class="col-5 border form-group">
+                                <small ><strong class="text-center text-dark-info font-weight-bold">PUESTO AL QUE POSTULA:  </strong> </small><br>{{$proceso->nombre}}
+                                <br>
+                                <small ><strong class="text-center text-dark-info font-weight-bold">AREA/OFICINA:  </strong> </small> <br>{{$proceso->oficina}}
+                            </div> 
+                        </div>                   
                     </div>
                     <div class="table-responsive">
                         <table class="table border-info table-bordered table-condensed">
@@ -47,14 +48,14 @@ Puede ver su postulación ingresando <a href="#">Aquí</a>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <th scope="row" class="alert alert-secondary">Apellidos y Nombres</th>
-                                    <td colspan="3">{{auth()->user()->apellido_paterno}} {{auth()->user()->apellido_materno}}, {{auth()->user()->nombres}}</td>
+                                    <th scope="row" class="alert alert-secondary">Nombres y Apellidos</th>
+                                    <td colspan="3">{{$datos_usuario->nombres}}, {{$datos_usuario->apellido_paterno}} {{$datos_usuario->apellido_materno}}</td>
                                 </tr>
                                 <tr>
                                     <th scope="row" class="alert alert-secondary">Documentos de Identidad</th>
                                     <td>{{auth()->user()->dni}}</td>
                                     <th scope="row" class="alert alert-secondary">RUC:</th>
-                                    <td>{{auth()->user()->dni}}</td>
+                                    <td>{{$datos_usuario->ruc}}</td>
                                 </tr>
                                 <tr>
                                     <td>3</td>
@@ -65,7 +66,12 @@ Puede ver su postulación ingresando <a href="#">Aquí</a>
                             </tbody>
                             
                         </table>
-                        <div>Puede ver su postulación ingresando <a href="#">Aquí</a></div>
+                    </div>
+                    
+                    <div>
+                        <h5 class="text-info mb-0">Se ha enviado una contancia de su postulación al correo registrado <strong class="text-success mb-0">{{$datos_usuario->email}}</strong><br>
+Puede ver su postulación ingresando <a href="#">Aquí</a>
+                        </h5>
                     </div>
                 </div>
             </div>
