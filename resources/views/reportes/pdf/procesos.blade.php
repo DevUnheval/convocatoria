4911e352cc9b03052e12d1cabf77f21b70306127
slayerderk <30374923+slayerderk@users.noreply.github.com>
2021-02-25 22:16:06 -0500

    ya quiere_PDF :)

--- resources/views/reportes/pdf/procesos.blade.php
+++ resources/views/reportes/pdf/procesos.blade.php
@@ -13,7 +13,7 @@
       left: 0px;
       top: -90px;
       right: 0px;
-      height: 80px;
+      height: 100px;
       background-color: #ddd;
       text-align: center;
     }
@@ -57,17 +57,39 @@
     <h1>
       <table>
         <tr  width="100%">
-          <td><img src="{{ asset('\imagenes\ajustes\logo.png')}}" alt="">
-          </td>
-          <td> texto</td>
+          <td> col 1: logo 1</td>
+          <td>"Año del Bicentenario del Perú: 200 años de Independencia"</td>
           <td style="text-align: right;"> col 3: logo 1</td>
         </tr>
       </table>
     </h1>
-    <h2>tema</h2>
-    <ul></ul>
-    
+    <h4>Proceso de Contratación CAS N° 00-2020</h4>
+    <label for=""><strong>NOMBRE DE LA CONVOCATORIA: </strong>ESPECIALISTA EN GESTIÓN INTERNA</label> <br>
+    <b style="color:blue">PUBLICACIÓN DE RESULTADOS DE EVALUACIÓN CURRICULAR</b>
+    <ul></ul> 
   </header>
+<br><br><br>
+    <table border="1" style="border solid">
+        <thead >
+            <tr>
+                <th rowspan="2">Orden de Mérito</th>
+                <th rowspan="2">Apellidos y Nombres</th>                                                    
+                <th rowspan="2">Puntaje</th>
+                <th colspan="2">Resultado</th>
+                <th colspan="2">Entrevista</th>                                             
+            </tr>
+            <tr>
+                <th>Califica</th>
+                <th>No Califica</th>
+                <th>Fecha</th>
+                <th>Hora</th>                                                  
+            </tr>
+        </thead>
+        <tbody id="zeroconfig3_body">
+                <!-- Cuerpo vacio -->                                                
+        </tbody>                                            
+    </table>
+
   <footer>
     <table>
       <tr>
@@ -84,11 +106,12 @@
       </tr>
     </table>
   </footer>
+  <br><br><br><br>
   <div id="content">
     <p>
       Lorem ipsum dolor sit...
     </p><p>
-        @for($i=0;$i<=500;$i++)
+        @for($i=0;$i<=300;$i++)
         Vestibulum pharetra fermentum fringilla  Vestibulum pharetra pharetra pharetra y la v ferment um fringilla ...<br>
         @endfor
         
